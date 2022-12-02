b3cc1cab42d3c9748afee441fb72d301276a669f
Irem Soyaker <irem_95@windowslive.com>
2022-12-02 21:01:50 +1100

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp2.Models;
+
+namespace JokesWebApp2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp2.Models.Joke> Joke { get; set; }
+    }
+}
